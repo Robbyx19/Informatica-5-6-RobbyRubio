9597970f1503bdc96a879ab45861bcb212f541ed
Robbyx19 <Robbyrubio19@gmail.com>
2025-08-26 16:41:15 +0000

    ´'kdafpomsp´}dsmf{s

---
+++ Checkpoint 3 / fizzbuzz.py
@@ -0,0 +1,4 @@
+numero = int(input("Ingresa un número entero: "))
+if 3 % == 0:    print ("fizz")
+else:
+print ("type another number")
---
+++ Checkpoint 3 /example.py
@@ -0,0 +1,7 @@
+def main():
+     n1= int (input("number 1: "))
+n2= int (input("number 2: "))
+n3= int (input("number 3: "))
+n4= int (input("number 4: "))
+if n1 > n2 and n1 > n3 and n1 > n4:
+elif n2 > n3 and 
