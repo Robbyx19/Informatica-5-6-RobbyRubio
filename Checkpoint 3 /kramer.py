717a4031c29d9b07b00622a53ab05047baa24b64
Robbyx19 <Robbyrubio19@gmail.com>
2025-08-27 20:20:54 +0000

    hndñlsajlkds

---
+++ Checkpoint 3 /kramer.py
@@ -0,0 +1,10 @@
+saludo = input ("escribe un saludo: ") .strip().lower ()
+def main():
+ while=True:
+    if saludo.startswith ("hola"):
+        print(0$)
+    elif saludo.startswith("h"):
+        print (20$)
+    else 
+        print (100$)
+    break 
