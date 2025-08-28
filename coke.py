7597f54f9798ebf4b055035ce2e5f98e4e6d153e
Robbyx19 <Robbyrubio19@gmail.com>
2025-08-28 17:32:58 +0000

    jlñadnñlsanñdlmas

---
+++ coke.py
@@ -0,0 +1,4 @@
+def main():
+    i = 25, 10 ,5
+i = int (input("insert a coin bro: "))
+print(" you have insert:",(i))
---
+++ jumps.py
@@ -0,0 +1,7 @@
+def main():
+    b= int(input("write a number: "))
+    while True: 
+        if b == 9:b < 9 
+        break 
+     
+main()
