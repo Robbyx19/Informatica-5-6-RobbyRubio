1e92d3e2b2fcf18829633c01fcfb50a9851707ae
Robbyx19 <Robbyrubio19@gmail.com>
2025-08-29 17:33:00 +0000

    hokidsjalkda

---
+++ Checkpoint 3 /life-loop.py
@@ -0,0 +1,10 @@
+def main():
+    import time 
+input (" que es algo que siempre a estado en tu cabeza en esta semana: ")
+subir_loma = False
+while True:
+    respuesta = input ("vas a subir la loma?   si/no: ").strip ().lower
+    if respuesta == ("si"):
+        print ("que bien ve a hacelooo! ")
+main()
+print if si
--- coke.py
+++ coke.py
@@ -1,4 +1,17 @@
 def main():
-    i = 25, 10 ,5
-i = int (input("insert a coin bro: "))
-print(" you have insert:",(i))
+        price = 50
+        total_paid = 0
+        vending_machine( price, total_paid )
+def vending_machine(price, total_paid):
+        while price > 0:
+    print(f"amount due: {price} ")
+pay = int(input("insert coin: "))
+if pay = 25: or pay == 10 or pay == 5:
+price = price - pay
+total_paid = total_paid + pay
+else 
+prin("not a coin ")
+if total_paid >= 50:
+   print(" thanks! here your coke")
+  print (f"here  your change : {total_paid - 50}")
+main()
