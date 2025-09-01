a5433b61c8d56a3455f756e69b6b9444d29bd618
Robbyx19 <Robbyrubio19@gmail.com>
2025-09-01 19:40:43 +0000

    gflkwejh fkjdsg lkfsd

---
+++ Checkpoint 3 /checkpoint 4/adv.strings.py
@@ -0,0 +1,21 @@
+#string input
+string_input = input("input a string: ")
+# print(f"You entered: {string_input}")
+
+# # print("-"*len(string_input))
+
+# # #string index
+# # print(f"Length of the string: {len(string_input)}")
+# # print(f"first character: {string_input[0]}")
+# # print(f"Last Character:{string_input[-1]}")
+
+
+
+# i = 0 
+# while i<len(string_input):
+#     print (string_input[i])
+#     i = i +-1
+print("h" in string_input)
+print ("j"in string_input)
+print("ello"in string_input)
+print(string_input.find("h"))
