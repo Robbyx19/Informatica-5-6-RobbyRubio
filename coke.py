2bfc478e2f121aaffb7b87ee510d7d29eb4c4f44
Robbyx19 <Robbyrubio19@gmail.com>
2025-09-04 20:20:43 +0000

    nr j,dsbg .j j,kds

--- coke.py
+++ coke.py
@@ -1,16 +1,23 @@
+def vending_machine(price, total_paid):
+    while price > 0:
+        print(f"Amount due: {price}")
+        try:
+            pay = int(input("Insert coin: "))
+            if pay == 25 or pay == 10 or pay == 5:
+                price -= pay
+                total_paid += pay
+            else:
+                print("Not a valid coin.")
+        except ValueError:
+            print("Please insert a valid number.")
+    
+    print("Thanks! Here's your coke.")
+    if total_paid > 50:
+        print(f"Here is your change: {total_paid - 50}")
+
 def main():
-        price = 50
-        total_pagado = 0
-        vending_machine( price, total_pagado )
-def vending_machine(price, total_pagado):
-        while price > 0:print(f"amount due: {price} ")
-pay = int(input("insert coin: "))
-if pay := 25:  pay == 10 or pay == 5
-price = price - pay
-total_pagado  = total_pagado + pay
-else
-print("not a coin ")
-if total_pagado >= 50:
-   print(" thanks! here your coke")
-print (f"here  your change : {total_pagado - 50}")
+    price = 50
+    total_paid = 0
+    vending_machine(price, total_paid)
+
 main()
