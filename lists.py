5ccf2870f593c47fba3257b5cf4f232d591d16dc
Robbyx19 <Robbyrubio19@gmail.com>
2025-09-05 17:22:36 +0000

    dsa d as

---
+++ lists.py
@@ -0,0 +1,19 @@
+independence_stages = ["inicio","Organizacion","Resistencia","Consumacion"]
+print (independence_stages [:4])
+print(len(independence_stages))
+leaders = []
+leaders.append("Miguel hidalgo")
+leaders.append("Jose Maria morelos")
+leaders.extend(independence_stages)
+print (leaders)
+independence_stages.append 
+leaders.insert (1,"Vicente Guerrero")
+leaders.remove ("Vicente Guerrero") 
+leaders.append ("agustin iturbide")
+leaders.append(input("type a leader"))
+leaders.clear
+leaders.count(" Vicente Guerrero ")
+leaders.sort
+leaders.reverse ()
+leaders.copy
+print(new_leaders)
