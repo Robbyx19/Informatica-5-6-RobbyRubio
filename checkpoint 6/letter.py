9150970035f786bbaa21fbcb7ad61f21bfa0d774
Robbyx19 <Robbyrubio19@gmail.com>
2025-09-18 16:56:36 +0000

    letter.py

--- checkpoint 6/letter.py
+++ checkpoint 6/letter.py
@@ -2,13 +2,13 @@ def main():
     list = ["Mario","Luigi","Daisy","Yoshi","Toad","Princess Peach","Bowser"]
     list.remove ("Princess Peach")
     for sender in list:
-        list(sender)
+        invitation(sender)
     
 
-def invitation(sender):
+def invitation(r):
     print(f"""
     +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-       Dear {sender},
+       Dear {r},
     
        You are cordially invited to a ball at
        Peach's Castle this evening, 7:00 PM.
