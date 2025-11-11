7675d4164338861825a8dcf0a3d07fba3e7f056f
Robbyx19 <Robbyrubio19@gmail.com>
2025-11-11 16:40:41 +0000

    FAIR

---
+++ universities.py
@@ -0,0 +1,10 @@
+universities = {
+    "eastern arizona ": {"majors":0, "cost":1000,"location":"arizona"},
+    "Tecmilenio": {"majors":27, "cost":10200,"location":"chihuahua "},
+    "TecMonterrey": {"majors":45, "cost":135000,"location":"monterrey "},
+    "ICATECH": {"majors":200, "cost":395 ,"location":"NCG "},
+    "CUCCI": {"majors":22, "cost":1500 ,"location":"chihuahua "},
+    "UTPAQUIME ": {"majors":9, "cost":1850 ,"location":"Paquime NCG "},
+
+    ""
+}
